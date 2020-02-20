bb4be5d49f1ce77336d7799d722c8bf07312b592
Michele Rawlins <shellbop@att.net>
2020-02-19 22:19:14 -0600

    added js and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# personal-bio-site
+## Description -- 
+This is a biography about me.  My work history, the technologies I have learned and the projects I have finished.
+
+
+## Screen shots
+![Main Screen](./screenshots/bioss.png)
+
+
+## Instructions how to run
+1.  Clone down the repo.
+2. Make sure you have http - server installed via npm.  If not, get it [here](https://www.npmjs.com/package/http-server)
+3. On your command line run `hs -p 9999`.
+4.  Navigate to local host `http://localhost:9999`
---
+++ main.js
@@ -0,0 +1,43 @@
+console.log("hello");
+
+const projects = [
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      }
+];
+
+const printToDom = (divId,textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+  }
+
+
+
+
+const createProjectCards = () => {
+    let projectsList = "";
+    let domString = "";
+    for(let i=0; i < projectsList.length; i++){
+    if (projectsList[i].available === 'true');
+domString += `<div id = "title">${projectsList[i].title}`;
+domString += `<img src = ${projectsList[i].screenshot}>`;
+domString += `<p> ${projectsList[i].description}</p>`;
+domString += `<p> ${projectsList[i].technologiesUsed}</p>`;
+domString += `<p> ${projectsList[i].url}</p>`;
+domString += `<p> ${projectsList[i].githubUrl}</p>`;
+
+}
+printToDom('projectsPage',domString )
+};
+const init = () => {
+
+    createProjectCards(projects);
+};
+
+init();
