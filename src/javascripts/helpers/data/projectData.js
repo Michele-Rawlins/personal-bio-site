b98cd7032a45180b69753b3770f8b2c077a3f88d
Michele Rawlins <shellbop@att.net>
2020-03-29 14:58:50 -0500

    added second project

--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,4 +1,5 @@
 import productCard from '../../../screenshots/productCards.png';
+import petAdopt from '../../../screenshots/PetAdoption2.png';
 
 const projects = [
   {
@@ -12,14 +13,14 @@ const projects = [
     githubUrl: 'https://github.com/Michele-Rawlins/product-cards',
   },
   {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    title: 'Pet Adopt',
+    screenshot: `${petAdopt}`,
+    description: 'This is my second project', // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
     // eslint-disable-next-line max-len
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    // url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: 'https://github.com/Michele-Rawlins/pet-adoption2',
   },
   {
     title: 'Cool Project',
