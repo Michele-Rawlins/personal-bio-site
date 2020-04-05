33ffdbb4ca0257c6c84b3e2276e55d63ec084ebd
Michele Rawlins <shellbop@att.net>
2020-04-04 22:27:29 -0500

    added json

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,12 +9,13 @@ console.error('hi');
 
 const createBio = () => {
   let domString = '';
-  domString += '<div>';
-  domString += `<img src="${photograph}" alt="black and white selfie">`;
+  domString += '<div id = "bioPage">';
+  domString += `<img class = "myPhoto" src="${photograph}" alt="black and white selfie">`;
   domString += '<p>Hello!!!  My name is Michele Rawlins.  Welcome to my page.</p>';
   domString += '<p> I have worked in manufacturing and the food industry for the last 32 years.</p>';
-
-
+  domString += '<p> My love of coding started when I was tasked with creating a soccer website for the club where my daughter played soccer, a few years ago.</p>';
+  domString += '<p> I started looking at a four year degree at Austin Peay when a friend told me about Nashville Software School.</p>';
+  domString += '<p>I signed up for a Jumpstart class and I was hooked.  I started coding and struggling and have never looked back.</p>';
   util.printToDom('bio-photo', domString);
 };
 
