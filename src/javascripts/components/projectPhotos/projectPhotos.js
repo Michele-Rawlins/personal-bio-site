e8afa9167e69d06adb6b7b70a72f58d8d1dbc4b1
Michele Rawlins <shellbop@att.net>
2020-05-11 22:48:22 -0500

    changed styling and bio location

---
+++ src/javascripts/components/projectPhotos/projectPhotos.js
@@ -0,0 +1,22 @@
+import productCardPhoto from '../../../screenshots/productCards.png';
+import PetAdoption from '../../../screenshots/PetAdoption2.png';
+import BearWatcher from '../../../screenshots/'
+
+
+cont projectPhotos = [
+  {
+    name: "Product Cards",
+    image: `${productCardPhoto}`,
+  },
+  {
+    name: "Pet Adoption",
+    image: `${PetAdoption}`,
+  }
+{
+  name: "Bear Watcher",
+    image: `${PetAdoption}`,
+}
+
+];
+
+export default { projectPhotos };
