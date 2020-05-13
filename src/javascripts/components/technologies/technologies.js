1c3d14629db3139a3bf6d8a1ebb78af5f879aefb
Michele Rawlins <shellbop@att.net>
2020-05-13 12:17:08 -0500

    added tech icons

--- src/javascripts/components/technologies/technologies.js
+++ src/javascripts/components/technologies/technologies.js
@@ -1,19 +1,28 @@
 import html from '../../../screenshots/html.png';
 import css from '../../../screenshots/css.png';
-
+import bootstrap from '../../../screenshots/bootstrap.png';
+import git from '../../../screenshots/git.png';
+import github from '../../../screenshots/github.png';
+import sass from '../../../screenshots/Sass.png';
+import webpack from '../../../screenshots/webpackpng.png';
+import react from '../../../screenshots/react.png';
 import util from '../../helpers/utils/utils';
 
 
 const technologies = () => {
   let domString = '';
-  domString += '<div id = "technologies">';
-  domString += '<h2> Technologies</h2>';
+  domString += '<div id="technologies">';
+  domString += '<h2 class="tech-header"> Technologies</h2>';
   domString += '<div class="technology-container">';
   domString += '<div class="row row-cols-4">';
-  domString += `<img class="col" src="${html}"></img>`;
-  domString += `<img class="col" src="${css}"></img>`;
-  domString += '<div class="col">Column</div>';
-  domString += '<div class="col">Column</div>';
+  domString += `<img class="col tech-icon" src="${html}"></img>`;
+  domString += `<img class="col tech-icon" src="${css}"></img>`;
+  domString += `<img class="col tech-icon" src="${bootstrap}"></img>`;
+  domString += `<img class="col tech-icon" src="${git}"></img>`;
+  domString += `<img class="col tech-icon" src="${github}"></img>`;
+  domString += `<img class="col tech-icon" src="${sass}"></img>`;
+  domString += `<img class="col tech-icon" src="${webpack}"></img>`;
+  domString += `<img class="col tech-icon" src="${react}"></img>`;
   domString += '</div>';
   domString += '</div>';
   util.printToDom('technologies', domString);
