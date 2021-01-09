1667727090ac099333d439334f6636d7ec14ffdf
Michele Rawlins <shellbop@att.net>
2021-01-09 15:53:37 -0600

    updated styling

--- src/javascripts/components/bio/bio.js
+++ src/javascripts/components/bio/bio.js
@@ -11,10 +11,10 @@ const createBio = () => {
   // domString += '<hr></hr>';
   // domString += '<p class="intro">Welcome to My Page</p>';
   domString += `<img class = "myPhoto" src="${photograph}" alt="black and white selfie">`;
-  domString += '<p class="bio-text">Hello!!!  My name is Michele Rawlins.  I am a Full Stack Web Developer.</p>';
-  domString += '<p class="bio-text">Current Skill Stack: Javascript, React, Sass, C#, ASPNET.MVC, SQL, .NetCore and an eternal thirst for knowledge. </p>';
-  domString += '<p class="bio-text"> I have worked in manufacturing and the food industry for the last 32 years.</p>';
-  domString += '<p class="bio-text">I look forward to discussing your vision. </p>';
+  domString += '<p class="bio-text">Hello!!!     My name is Michele Rawlins.     I am a Full Stack Web Developer.    Current Skill Stack: Javascript, React, Sass, C#, ASPNET.MVC, SQL, .NetCore and an eternal thirst for knowledge.  I have worked in manufacturing and the food industry for the last 32 years.  I look forward to discussing your vision.</p>';
+  // domString += '<p class="bio-text">Current Skill Stack: Javascript, React, Sass, C#, ASPNET.MVC, SQL, .NetCore and an eternal thirst for knowledge. </p>';
+  // domString += '<p class="bio-text"> I have worked in manufacturing and the food industry for the last 32 years.</p>';
+  // domString += '<p class="bio-text">I look forward to discussing your vision. </p>';
   // domString += '<p class="bio-text"> My love of coding started when tasked with creating a soccer website for the club where my daughter played soccer, a few years ago.</p>';
   // domString += '<p class="bio-text"> A friend told me about Nashville Software School.</p>';
   // domString += '<p class="bio-text">I signed up for a Jumpstart class and was hooked.  I started coding and have never looked back.</p>';
